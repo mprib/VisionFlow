feda14de93f57dbacdcca91a0f398f69b377db09
mprib <prible@gmail.com>
2023-08-16 17:08:27 -0500

    log fps on server

--- visionflow/server.py
+++ visionflow/server.py
@@ -2,6 +2,7 @@ from visionflow.log_manager import get
 logger = get(__name__)
 
 import cv2
+import time
 import socket
 import struct
 import numpy as np
@@ -31,10 +32,13 @@ print(f"Server listening on {host}:{port}")
 
 client_socket, client_address = server_socket.accept()
 print(f"Connection from: {client_address}")
-
+frame_count = 0
+tic = time.time()
 while True:
     ret, frame = capture.read()
-
+    frame_count +=1
+    toc=time.time()
+    fps = frame_count/(toc-tic)
     # convert frame to bytes
     frame = frame.astype(np.uint8)  # Ensure data is of type uint8
     # Serialize the frame as bytes
@@ -43,8 +47,8 @@ while True:
     # Get the frame shape for reconstruction
     
     frame_shape = frame.shape
-    logger.info(f"Frame shape is: {frame_shape}")
-
+    logger.info(f"Frame shape is {frame_shape}...reading on average at {fps} fps")
+     
     # package shape too
     header = struct.pack('<LIII', frame_size, *frame.shape)
 
