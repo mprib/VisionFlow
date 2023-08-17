f68ed5fd967a14726d90f1db0a932cfc53846217
mprib <prible@gmail.com>
2023-08-17 16:45:23 -0500

    some friendly help from chatgpt

--- dev/hist/test_camera_read.py
+++ dev/hist/test_camera_read.py
@@ -1,13 +1,24 @@
 import cv2
 import numpy as np
+import platform
 
 from pathlib import Path
 
-# Create a capture object for the webcam
-capture = cv2.VideoCapture(0, cv2.CAP_ANY)
 
+if platform.system() == "Windows":
+    connection_method = cv2.CAP_DSHOW
+else:
+    connection_method = cv2.CAP_ANY
+
+# Create a capture object for the webcam
+capture = cv2.VideoCapture(0, connection_method)
+width = 1280
+height = 720
 # collect a test frame
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 success, frame = capture.read()
+
 
 # Check if the capture object was successfully opened
 if not capture.isOpened():
@@ -20,9 +31,9 @@ while True:
     ret, frame = capture.read()
     if ret:
         print(f"A frame of size {frame.shape} is being read")
-    #cv2.imshow("Webcam", frame)
+    cv2.imshow("Webcam", frame)
     
-    #if cv2.waitKey(1) ==ord("q"):
-    #    cv2.destroyAllWindows()
-    #    break
+    if cv2.waitKey(1) ==ord("q"):
+       cv2.destroyAllWindows()
+       break
 
